e0b6b90d50a387a3304e0ab323a2edd6bf815059
lukasmoldon <49486044+lukasmoldon@users.noreply.github.com>
2020-10-31 12:23:06 +0100

    uniform naming, improved performance, added counter

--- api_nyt.py
+++ api_nyt.py
@@ -55,24 +55,40 @@ def download_articles(api_key, path_results, start, end):
 
 def load_articles(path_data):
     with open(path_data) as fp:
-        article_data = json.load(fp)
-    return article_data
+        articles = json.load(fp)
+    return articles
 
 
 
-def get_article(article_data, year, month, count, article_property):
-    return article_data[str(year)][str(month)][count][article_property]
+def get_article(articles, year, month, count, article_property):
+    return articles[str(year)][str(month)][count][article_property]
 
 
 
-def get_Attributes(articles, attribute):
-    # Input articles from download_articles and an attribute like "abstract"
-    # Returns a list of all attributes of the given articles
-    attributes = []
+def get_attributes(articles, attribute):
+    # Input: articles from download_articles and an attribute like "abstract"
+    # Returns: list of all attributes of the given articles
+    attributes = set()
     for y in articles.keys():
         for m in articles[y].keys():
             for a in articles[y][m]:
-                attributes.append(articles[y][m][a][attribute])
+                attributes.add(articles[y][m][a][attribute])
+    return attributes
+
+
+
+def count_attributes(articles, attribute):
+    # Input: articles from download_articles and an attribute like "abstract"
+    # Returns: counts all attributes of the given articles
+    attributes = {}
+    for y in articles.keys():
+        for m in articles[y].keys():
+            for a in articles[y][m]:
+                val = articles[y][m][a][attribute]
+                if val in attributes:
+                    attributes[val] += 1
+                else:
+                    attributes[val] = 1
     return attributes
 
 
--- api_theguardian.py
+++ api_theguardian.py
@@ -69,14 +69,42 @@ def download_articles(api_key, path_results, start, end):
 
 
 def load_articles(path_data):
-    with open("/home/lmoldon/forschungspraktikum/theguardian.json", "r") as fp:
-        article_data = json.load(fp)
-    return article_data
+    with open(path_data) as fp:
+        articles = json.load(fp)
+    return articles
 
 
 
-def get_article(article_data, year, month, count, article_property):
-    return article_data[str(year)][str(month)][count][article_property]
+def get_article(articles, year, month, count, article_property):
+    return articles[str(year)][str(month)][count][article_property]
+
+
+
+def get_attributes(articles, attribute):
+    # Input: articles from download_articles and an attribute like "abstract"
+    # Returns: list of all attributes of the given articles
+    attributes = set()
+    for y in articles.keys():
+        for m in articles[y].keys():
+            for a in articles[y][m]:
+                attributes.add(articles[y][m][a][attribute])
+    return attributes
+
+
+
+def count_attributes(articles, attribute):
+    # Input: articles from download_articles and an attribute like "abstract"
+    # Returns: counts all attributes of the given articles
+    attributes = {}
+    for y in articles.keys():
+        for m in articles[y].keys():
+            for a in articles[y][m]:
+                val = articles[y][m][a][attribute]
+                if val in attributes:
+                    attributes[val] += 1
+                else:
+                    attributes[val] = 1
+    return attributes
 
 
 
