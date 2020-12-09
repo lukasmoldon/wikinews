2e5d0286e17145711ad72f0c57551b991ca27e05
lukasmoldon <49486044+lukasmoldon@users.noreply.github.com>
2020-12-09 18:58:40 +0100

    Added function to match a set of different keywords at the same time

--- matching.py
+++ matching.py
@@ -38,8 +38,39 @@ def match(keyword, articles, restore_keyword=False, date=None, cooc_daterange=No
     matching = wiki.search_articles(query, nmax=nmax, date=date, timeout=timeout)
     return matching
 
-def groupmatch(keywords, articles, dates=None, cooc_daterange=None, cooc_wordcount=2, nmax=1, timeout=10, useAbstract=True):
-    pass # TODO
+def groupmatch(keywords, articles, restore_keyword=False, dates=None, cooc_daterange=None, cooc_wordcount=2, nmax=1, timeout=10, useAbstract=True):
+    matching = {}
+    done = set()
+    for keyword in keywords:
+        skip = False
+        for el in done:
+            if keyword in el:
+                skip = True
+        if not skip:
+            if cooc_daterange == None:
+                cooc = mng.get_cooccurrences(keyword, articles, useAbstract=useAbstract)
+                if restore_keyword:
+                    restored = mng.restore_keyword(keyword, articles)[0][0]
+            else:
+                start = date - timedelta(days=cooc_daterange)
+                end = date + timedelta(days=cooc_daterange)
+                cooc = mng.get_cooccurrences(keyword, articles, start=start, end=end, useAbstract=useAbstract)
+                if restore_keyword:
+                    restored = mng.restore_keyword(keyword, articles, start=start, end=end, useAbstract=useAbstract)[0][0]
+            cooc = cooc[:cooc_wordcount]
+            if restore_keyword:
+                query = set(restored.split())
+                for el in cooc:
+                    query.add(el[0])
+                query = " ".join(query)
+            else:
+                query = keyword
+                for el in cooc:
+                    query += " " + el[0]
+            done.add(query)
+            matching[keyword] = wiki.search_articles(query, nmax=nmax, date=date, timeout=timeout)
+    return matching
+    
 
 #nyt2019 = mng.load_articles("C:/Users/lukas/Documents/GitHub/wikinews/nyt2019.json")
 #print(match("trump", nyt2019))
