a66bdd549b627c62899303da70ee6dd4fefa7683
lukasmoldon <49486044+lukasmoldon@users.noreply.github.com>
2020-12-21 18:35:09 +0100

    Bugfix + docstrings for graphics class

--- graphics.py
+++ graphics.py
@@ -3,7 +3,32 @@ import api_wikipedia as wiki
 from datetime import date
 
 def plot_wikipedia(title, start, end, language_edition="en"):
-    x, y = wiki.get_counts(title,start,end)
+    """
+    Plot daily wikipedia pageview counts for a given article.
+
+    Plots the daily wikipedia pageview counts between days ``start`` and ``end`` for an article with given ``title``.
+
+    Notes
+    -----
+    We only consider english wikipedia articles in our analysis.
+
+    Parameters
+    ----------
+    title : str
+        Title of the wikipedia article (https://en.wikipedia.org/wiki/``title``).
+    start : datetime.date
+        First day of plot.
+    end : datetime.date
+        Last day of plot.
+    language_edition : str
+        Selector for the language edition of wikipedia (default is ``en`` for english, see Notes).
+    
+    Returns
+    -------
+    None
+
+    """
+    x, y = wiki.get_counts(title,start,end, language_edition=language_edition)
     if x != [] and y != []:
         plt.figure(figsize=(10,10))
         plt.plot(x,y)
@@ -11,6 +36,23 @@ def plot_wikipedia(title, start, end, language_edition="en"):
         plt.show()
 
 def plot_dataset(word,dataset,timepoints):
+    """
+    Plot any data with finite amount of datapoints.
+
+    Parameters
+    ----------
+    word : str
+        Title of the plot (e.g. keyword).
+    dataset: list of int
+        Values for the plot (y axis).
+    timepoints: list of Timepoint
+        Timepoints for the plot (x axis).
+    
+    Returns
+    -------
+    None
+
+    """
     print(timepoints)
     if timepoints != [] and dataset != []:
         plt.figure(figsize=(10,10))
