25b18d6241db5606a6c13b021987c8584b95fd45
lukasmoldon <49486044+lukasmoldon@users.noreply.github.com>
2020-12-23 02:30:51 +0100

    Update TextPreprocessing.py

--- TextPreprocessing.py
+++ TextPreprocessing.py
@@ -14,24 +14,50 @@ stemmer = LancasterStemmer()
 #stemmer = PorterStemmer()
 
 def stemSentence(sentence):
+    """
+    Apply stemming on a given ``sentence`` (see ``nltk.stem.lancaster``).
+
+    """
     return stemmer.stem(sentence)
 
 def removeStopwords(sentence):
+    """
+    Tokenizes a given ``sentence`` (see ``nltk.tokenize``) and removes stopwords from it (see ``nltk.corpus.stopwords``).
+
+    We also remove custom selected stopwords: ["‘", "’","-",".","”","“",'.',"new","said",'—'].
+
+    """
     tokens = word_tokenize(sentence)
     customStopwords = ["‘", "’","-",".","”","“",'.',"new","said",'—']
     allStopwords = stopwords.words() + word_tokenize(string.punctuation) + customStopwords
     return [w for w in tokens if w not in allStopwords]
 
 def removeNumbers(sentence):
+    """
+    Removes numbers from a given ``sentence``.
+
+    """
     return re.sub(r'\d+', '', sentence)
 
 def parseSentence(sentence):
+    """
+    Applys ``removeNumbers``, ``stemSentence`` and ``removeStopwords`` on a given ``sentence``.
+
+    """
     return removeStopwords(stemSentence(removeNumbers(sentence)))
 
 def wordfreq(word):
+    """
+    Returns the frequency of a word in the English language, based on many sources of data (see https://pypi.org/project/wordfreq/).
+
+    """
     return zipf_frequency(word,'en')
 
 def getNames(word):
+    """
+    Returns and filters the entity type of a word in the English language (see https://pypi.org/project/spacy/).
+
+    """
     names = []
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(word)
@@ -39,9 +65,8 @@ def getNames(word):
         if token.ent_type_ == "PERSON" or token.pos_ == "PROPN":
             names.append(token.text)
     return names
-
-
-'''Possible tags according to https://medium.com/@gianpaul.r/tokenization-and-parts-of-speech-pos-tagging-in-pythons-nltk-library-2d30f70af13b
+    '''
+    Possible tags according to https://medium.com/@gianpaul.r/tokenization-and-parts-of-speech-pos-tagging-in-pythons-nltk-library-2d30f70af13b
     CC coordinating conjunction
     CD cardinal digit
     DT determiner
@@ -76,16 +101,30 @@ def getNames(word):
     WDT wh-determiner which
     WP wh-pronoun who, what
     WP$ possessive wh-pronoun whose
-    WRB wh-abverb where, when'''
-
+    WRB wh-abverb where, when
+    '''
 
 def getNouns(words):
+    """
+    Returns all nouns of the list ``words``.
+
+    """
     nounTags = ["NN","NNS","NNP","NNPS","FW"]
     return tagWords(words, nounTags)
+
 def getVerbs(words):
+    """
+    Returns all verbs of the list ``words``.
+
+    """
     verbTags = ["VBD","VBG","VBN","VBP","VBZ"]
     return tagWords(words,verbTags)
+
 def tagWords(words, tags):
+    """
+    Computes tags for all ``words`` and optionally applies a filter which is a list of tags.
+
+    """
     txt = " ".join(words)
     tagged = nltk.pos_tag(nltk.word_tokenize(txt))
     if len(tags) == 0:
