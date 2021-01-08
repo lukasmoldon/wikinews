4a28d4cab6f97bda298555eb3c5dc3f13249a2e9
lukasmoldon <49486044+lukasmoldon@users.noreply.github.com>
2021-01-08 14:41:35 +0100

    Converted multithreading approach to a multiprocessing approach

--- multithreading.py
+++ multithreading.py
@@ -17,41 +17,41 @@ import numpy
 import copy
 from copy import deepcopy
 import logging
-import concurrent.futures
+import multiprocessing as mp
 
 log_starttime = datetime.datetime.now()
-logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.DEBUG)
 
-results_random = []
-result_original = None
-articles_original = None
+output = mp.Queue()
 
-def single_run(pid):
+def single_run(pid, articles):
     logging.info("Starting PID {}".format(pid))
-    global articles_original
-    if pid == 0:
-        # compute correlation of original data
-        articles = articles_original.copy()
-    else:
+    if pid > 0:
         # create random sample and compute correlation
-        articles = mng.shuffle_publicationdates(articles_original)
+        articles = mng.shuffle_publicationdates(articles)
+    logging.debug("PID {}: A".format(pid))
     #Get a dict of dicts for each calendar week with word frequencies from getWordCounts
     wordCounts = mng.getWordCounts(articles)
     #List of distinct words 
     distinctWords = mng.getDistinctWords(wordCounts)
+    logging.debug("PID {}: B".format(pid))
     #List of lists of tuples containing weekly word frequency
     countsPerWeek = []
     for w in distinctWords:
         countsPerWeek.append((w,mng.getCountPerWeek(wordCounts,w)))
+    logging.debug("PID {}: C".format(pid))
     #Create list of word objects for each keyword
     words = []
     for c in countsPerWeek:
         words.append(wpy.Word(c[0],ts_articles=timeseries.Timeseries(c[1])))
+    logging.debug("PID {}: D".format(pid))
     interestingWords = mng.filter_interestingness(articles, 10, 5)
+    logging.debug("PID {}: E".format(pid))
     keywords = []
     for k in interestingWords:
         keywords.append(k)
     m = matching.groupmatch(keywords, articles)
+    logging.debug("PID {}: F".format(pid))
     for key in m.keys():
         #Following line from 
         #https://stackoverflow.com/questions/7125467/find-object-in-list-that-has-attribute-equal-to-some-value-that-meets-any-condi
@@ -67,7 +67,9 @@ def single_run(pid):
                 word.ts_wiki = timeseries.parseWikipediaCounts(wiki_counts)
         except:
             print("ERROR: Key is invalid")
+    logging.debug("PID {}: G".format(pid))
     words_analyze = [x for x in words if x.wikipediaSite != ""]
+    logging.debug("PID {}: H".format(pid))
     #Drop all timepoints which are not contained in both article and wikipedia timeseries
     corr = []
     for w in words_analyze:
@@ -79,35 +81,36 @@ def single_run(pid):
             words_analyze.remove(w)
         else:
             corr.append(statistics.getCorrelation(ts_a.getCounts(),ts_w.getCounts()))
-    if pid == 0:
-        global result_original
-        result_original = numpy.mean(corr)
-    else:
-        global results_random
-        results_random.append(numpy.mean(corr))
+    logging.debug("PID {}: I".format(pid))
+    output.put([pid,numpy.mean(corr)])
     logging.info("Finished PID {}".format(pid))
 
 def run(n, max_workers=72):
     logging.info("Loading initial data ...")
     # Load all articles from 2019
-    global articles_original
     articles_original = mng.load_articles("nyt2019.json")
     logging.info("Done.")
 
     logging.info("Starting ...")
-    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-        executor.map(single_run, range(n+1))
+    processes = [mp.Process(target=single_run, args=(pid, deepcopy(articles_original))) for pid in range(n+1)]
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
+    results = [output.get() for p in processes]
     logging.info("Done with all tasks.")
-
-    global result_original
-    print("Mean correlation in original data: {}".format(result_original))
-    global results_random
-    print("Mean correlation in {} random samples: {}".format(n-1, statistics.mean_confidence_interval(results_random)))
+    random_collection = []
+    for el in results:
+        if el[0] == 0:
+            print("Mean correlation in original data: {}".format(el[1]))
+        else:
+            random_collection.append(el[1])
+    print("Mean correlation in {} random samples: {}".format(n-1, statistics.mean_confidence_interval(random_collection)))
     print("Correlation of each sample:")
-    print(results_random)
+    print(random_collection)
 
 
-run(1000)
+run(10)
 
 log_endtime = datetime.datetime.now()
 log_runtime = (log_endtime - log_starttime)
